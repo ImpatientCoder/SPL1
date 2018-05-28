e06e43e5926c843ac6054296ae909c05d98e109a
ImpatientCoder <36191761+ImpatientCoder@users.noreply.github.com>
2018-05-29 03:26:56 +0600

    Add files via upload

--- PhotoEditingFeatures.java
+++ PhotoEditingFeatures.java
@@ -1,39 +1,126 @@
 package spl1;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.DisplayMode;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class PhotoEditingFeatures
-{
+
+
+public class PhotoEditingFeatures extends JPanel implements ActionListener
+{	
+
+	
+	public JFrame frame;
+	
+	Timer timer;
+	int delay = 10;
+	int x = 200, y = 0, directionX=2, directionY=2;
+	
+	boolean seeAnimation=false;	
+	public BufferedImage image;
+	
+	
 			
-	public PhotoEditingFeatures()
-	{
+	public PhotoEditingFeatures(JFrame frame)
+	{	
+		this.frame=frame;
+		timer = new Timer(delay,this);
+		timer.start();
 
 	}
 	
+	
+		
+	
+	public void paintComponent(Graphics g)
+	{	
+		g.fillRect(0,0, 700, 700);
+		g.drawImage(image,x,y,300,300,null);		
+	}
 
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{	
+		if(seeAnimation)
+		{
+			
+			x+=directionX;
+			y+=directionY;
+			
+			if(x<=0 || x>=400)
+			{	
+				directionX*=-1;
+			}
+			if(y<=0 || y>=400)
+			{	
+				directionY*=-1;
+			}
+			
+		}
+		repaint();		
+	}
+
+
+
+	
+	
+	
+	
+	public void makeAnimation(BufferedImage image)
+	{
+		
+		if(image == null)
+		{
+		   System.out.println("Please, open the File Menu \n And Select an image to edit...\n\n");
+		   return;
+		}
+		
+		
+		this.image=image;	
+		seeAnimation=true;
+		
+	}
+	
+	
+
+	
+	
+	
 	public void makeNegative(BufferedImage image)
 	{
+		
+			if(image == null)
+			{
+				   System.out.println("Please, open the File Menu \n And Select an image to edit...\n\n");
+				   return;
+			}
+				
 				    
-		   int hight = image.getHeight();	//get image width and height
-		   int width = image.getWidth();
+		   int row = image.getHeight();	//get image width and height
+		   int column = image.getWidth();
 		    
-		   for(int i = 0; i < hight; i++)
+		   for(int i = 0; i < row; i++)
 		   {
-		      for(int j = 0; j < width; j++)
+		      for(int j = 0; j < column; j++)
 		      {
 		    	  
 		        int pixel = image.getRGB(j, i);
@@ -62,16 +149,24 @@ public class PhotoEditingFeatures
 		      System.out.println(e);
 		    }	   
 		  
-		}
+	}
+	
+
+	
+	
 	
 	
 	public void makeMirror(BufferedImage image)
 	{
-	    int count = 0;
+		if(image == null)
+		{
+		   System.out.println("Please, open the File Menu \n And Select an image to edit...\n\n");
+		   return;
+		}
+			
 	    int column = image.getWidth();  //width means how many columns are in...(pixels in horizontal)
 	    int	row = image.getHeight();	//hight means how many rows are in...(pixels in vertical)
 	    
-	    System.out.println(column+"	"+row);
 	    
 	  //Create pixel Array
 		
@@ -81,12 +176,10 @@ public class PhotoEditingFeatures
 		{
 		    for( int j = 0; j < row; j++ )
 		    {	    	
-		    	pixels[i][j] = image.getRGB(i, j);		    	
-		    	count++;	
+		    	pixels[i][j] = image.getRGB(i, j);		    		
 		    }
 		}		
-		System.out.println(count);
-		
+	
 		//image format (column*row) pixel
 		//width(refers column number) 0 1 2 3...
 		//hight(refers row number)	  (0,0),(0,1), (0,2)...up to down	
@@ -115,58 +208,80 @@ public class PhotoEditingFeatures
 		 
 	}
 	
-//Create pixel Array
+
 	
-//	int[][] pixels = new int[w][h];
-//
-//	for( int i = 0; i < w; i++ )
-//	    for( int j = 0; j < h; j++ )
-//	        pixels[i][j] = img.getRGB( i, j );
-//	System.out.println(pixels[i][j]);
+	
 	
 	public void rotateImage(BufferedImage image)
 	{
 		
-	    int column = image.getWidth();
-	    int	row = image.getHeight();
-	    
-		int[][] pixels = new int[column][row];
-		
-		for( int i = 0; i < column; i++ )
+		if(image == null)
 		{
-		    for( int j = 0; j < row; j++ )
+			   System.out.println("Please, open the File Menu \n And Select an image to edit...\n\n");
+			   return;
+		}
+		
+		
+		
+	    int width = image.getWidth();
+	    int	hight = image.getHeight();
+	    
+		int[][] pixels = new int [hight][width];
+		
+		
+		
+		for( int i = 0; i < hight; i++ )
+		{
+		    for( int j = 0; j < width; j++ )
 		    {	    	
 		    	pixels[i][j] = image.getRGB(i, j);		    		
 		    }
-		 }
+		}
 		
-		int[][] rotatePixels = new int[row][column];
 		
-		for( int i = 0; i < column; i++ )
+		
+		int[][] rotatePixels = new int[width][hight];
+		
+		for( int i = 0; i < width; i++ )
 		{
-		    for( int j = 0; j < row; j++ )
-		    {	    	
-		    	rotatePixels[j][i] = pixels[i][row-1-j];		//??????????? //???????(0,row-1) no pixel goes to (0, 0)    // then (0,row-2)....(1,0)		
+		    for( int j = 0; j < hight; j++ )
+		    {	    
+		    	rotatePixels[i][j] = pixels[hight-j-1][i];
+		    	image.setRGB(i, j, rotatePixels[i][j]);
 		    }
 		}
 		
-		for( int i = 0; i < column; i++ ) 
-		{
-			    for( int j = 0; j < row; j++ )
-			    {
-			        image.setRGB(i, j, rotatePixels[j][i]);
-			    }
-			    
-		 }
+		
+		
+//		for( int i = 0; i < hight; i++ ) 
+//		{
+//			    for( int j = 0; j < width; j++ )
+//			    {
+//			        image.setRGB(i, j, rotatePixels[j][i]);
+//			    }
+//			    
+//		 }
+		
 		
 	
 	}
 	
+
 	
-	public void makeBlackWhite(BufferedImage image)
+	
+	
+	public void makeBlackWhite(BufferedImage image) throws IOException
 	{
+		
+		if(image == null)
+		{
+			   System.out.println("Please, open the File Menu \n And Select an image to edit...\n\n");
+			   return;
+		}
+		
 	    int column = image.getWidth();
 	    int	row = image.getHeight();
+	    
 	    
 		for( int i = 0; i < column; i++ ) 
 		{
@@ -174,29 +289,31 @@ public class PhotoEditingFeatures
 			    {
 			        int pixel  = image.getRGB(i, j);
 			        
-			        int alpha = (pixel>>24) & 0xff;
-			        int red	  = (pixel>>16) & 0xff;
-			        int green = (pixel>>8)  & 0xff;
+			        System.out.print((pixel >>16) & 0xff);
+			        
+			        int alpha = (pixel >>24) & 0xff;
+			        int red	  = (pixel >>16) & 0xff;
+			        int green = (pixel >>8)  & 0xff;
 			        int blue  =  pixel & 0xff;
 			        
 			        int average = (red + green + blue)/3;
+//			        
+//			        red = average;
+//			        green = average;
+//			        blue = average;
 			        
-			        red = average;
-			        green = average;
-			        blue = average;
-			        
-//			        if( (average*2) > 255)
-//			        {
-//			        	red   = 255; 
-//			        	green = 255;
-//			        	blue  = 255;
-//			        }
-//			        else
-//			        {
-//			        	red   = 0; 
-//			        	green = 0;
-//			        	blue  = 0;			        	
-//			        }
+			        if( (average*2) > 255)
+			        {
+			        	red   = 255; 
+			        	green = 255;
+			        	blue  = 255;
+			        }
+			        else
+			        {
+			        	red   = 0; 
+			        	green = 0;
+			        	blue  = 0;			        	
+			        }
 			        
 			        pixel = (alpha<<24)| (red<<16)| (green<<8)| blue;			        
 			        image.setRGB(i, j, pixel);
@@ -205,11 +322,179 @@ public class PhotoEditingFeatures
 			    
 		 }
 		
-		
-		
+				
 	}
+
+	
 	
 
 	
+	
+	
+	public void makeEdgeImage(BufferedImage image)
+	{
+		   if(image == null)
+		   {
+			   System.out.println("Please, open the File Menu \n And Select an image to edit...\n\n");
+			   return;
+		   }
+		   
+		   int w = image.getWidth();
+		   int h = image.getHeight();
+		
+		
+		//Create pixel Array
+		
+		int[][] pixels = new int[w][h];
 
+		for( int i = 0; i < w; i++ )
+		{
+		    for( int j = 0; j < h; j++ )
+		    {
+		        pixels[i][j] = image.getRGB(i, j);
+				System.out.println(pixels[i][j]);
+		    }
+		}    
+
+		   
+		   
+	}
+
+	
+	
+	
+	
+	public void makeRedImage(BufferedImage image)
+	{
+		
+		
+		   if(image == null)
+		   {
+			   System.out.println("Please, open the File Menu \n And Select an image to edit...\n\n");
+			   return;
+		   }
+		
+				    
+		   int row = image.getHeight();		//get image width and height
+		   int column = image.getWidth();
+		   
+		   
+		    
+		   for(int i = 0; i < row; i++)
+		   {
+		      for(int j = 0; j < column; j++)
+		      {
+		    	  
+		        int pixel = image.getRGB(j, i);
+		        
+		        int alpha = (pixel>>24) & 0xff;
+		        int red	  = (pixel>>16) & 0xff;
+		        int green = (pixel>>8)  & 0xff;
+		        int blue  =  pixel & 0xff;
+		      
+		        red   = 255 ;
+
+		       
+		        pixel = (alpha<<24) | (red<<16) | (green<<8) | blue;  //subtract RGB from 255 //convert to negative
+		        image.setRGB(j, i, pixel);
+		      }
+		      
+		   }
+		  	  
+	 }
+
+
+	
+	
+	
+
+	public void makeBlueImage(BufferedImage image)
+	{
+		
+		
+		   if(image == null)
+		   {
+			   System.out.println("Please, open the File Menu \n And Select an image to edit...\n\n");
+			   return;
+		   }
+		
+				    
+		   int row = image.getHeight();		//get image width and height
+		   int column = image.getWidth();
+		   
+		   
+		    
+		   for(int i = 0; i < row; i++)
+		   {
+		      for(int j = 0; j < column; j++)
+		      {
+		    	  
+		        int pixel = image.getRGB(j, i);
+		        
+		        int alpha = (pixel>>24) & 0xff;
+		        int red	  = (pixel>>16) & 0xff;
+		        int green = (pixel>>8)  & 0xff;
+		        int blue  =  pixel & 0xff;
+		      
+		        blue   = 255 ;
+
+		       
+		        pixel = (alpha<<24) | (red<<16) | (green<<8) | blue;  //subtract RGB from 255 //convert to negative
+		        image.setRGB(j, i, pixel);
+		      }
+		      
+		   }
+		  	  
+	 }
+	
+	
+	
+	
+	
+
+	public void makeGreenImage(BufferedImage image)
+	{
+		
+		
+		   if(image == null)
+		   {
+			   System.out.println("Please, open the File Menu \n And Select an image to edit...\n\n");
+			   return;
+		   }
+		
+				    
+		   int row = image.getHeight();		//get image width and height
+		   int column = image.getWidth();
+		   
+		   
+		    
+		   for(int i = 0; i < row; i++)
+		   {
+		      for(int j = 0; j < column; j++)
+		      {
+		    	  
+		        int pixel = image.getRGB(j, i);
+		        
+		        int alpha = (pixel>>24) & 0xff;
+		        int red	  = (pixel>>16) & 0xff;
+		        int green = (pixel>>8)  & 0xff;
+		        int blue  =  pixel & 0xff;
+		      
+		        green   = 255 ;
+
+		       
+		        pixel = (alpha<<24) | (red<<16) | (green<<8) | blue;  //subtract RGB from 255 //convert to negative
+		        image.setRGB(j, i, pixel);
+		      }
+		      
+		   }
+		   
+		  	  
+	 }	
+
+
+	
+	
+		
+		
 }
